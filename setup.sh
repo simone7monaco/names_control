dbe6490cdc339637af9a1528180505ed4c973f20
simone7monaco <simone7monaco@gmail.com>
2022-01-12 19:07:07 +0100

    Change script name

--- setup.sh
+++ setup.sh
@@ -19,9 +19,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Installing..."
 # python -m pip install -r $SCRIPT_DIR/requirements.txt
 # streamlit run $SCRIPT_DIR/web_app.py
-mkdir -p s/Applications/Controlla_Nomi.app/Contents/MacOS
+mkdir -p /Applications/Controlla_Nomi.app/Contents/MacOS
 
-cp run.sh /Applications/Controlla_Nomi.app/Contents/MacOS/Controlla_Nomi
+cp $SCRIPT_DIR/run.sh /Applications/Controlla_Nomi.app/Contents/MacOS/Controlla_Nomi
 chmod +x /Applications/Controlla_Nomi.app/Contents/MacOS/Controlla_Nomi
-cp Controller.py /Applications/Controlla_Nomi.app/Contents/MacOS/Cotroller.py
-cp hotwords.yaml /Applications/Controlla_Nomi.app/Contents/MacOS/
+cp $SCRIPT_DIR/Controller.py /Applications/Controlla_Nomi.app/Contents/MacOS/Cotroller.py
+cp $SCRIPT_DIR/hotwords.yaml /Applications/Controlla_Nomi.app/Contents/MacOS/
